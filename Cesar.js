9133a33978657646f661ee2fd0d993267992046b
ahmed oueslati <oueslatiahmed38@gmail.com>
2025-07-06 23:23:27 +0200

    msg

--- Cesar.js
+++ Cesar.js
@@ -5,16 +5,27 @@ const alphabet = [
   'n','o','p','q','r','s','t','u','v','w','x','y','z'
 ];
 let  phrase= process.argv[2];
+
 const shiftNumber = process.argv[3];
 
 function cesar (){
+
+       if (!phrase){
+    console.log("Please write your message and your shift number"); } 
+
     let result = '';
-    let char = [alphabet];
-   if (!phrase){
-    console.log("Please write your message and your shift number");
-   } 
+    let char =  char.toLowerCase() ;
 
+        for( i=0 ; i < alphabet.length ; i++ );
+        const index = alphabet.indexOf(char);
+        const shiftedIndex =  (index + shiftNumber)%26;
 
+        if (shiftNumber < 0 ){
+            shiftedIndex += 26 ; }
+  let newChar =  alphabet[shiftedIndex];
+  result += newChar;
+   
+  console.log("Encrypted message:", result);
 }
 
 
